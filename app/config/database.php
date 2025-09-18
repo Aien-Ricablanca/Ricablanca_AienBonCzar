79bdcd071305e41639c22d2b0c9b58a50ccbbe94
Aien-Ricablanca <aienbonczarricablanca@gmail.com>
2025-09-19 03:10:26 +0800

    update

--- app/config/database.php
+++ app/config/database.php
@@ -59,11 +59,11 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 $database['main'] = array(
     'driver'	=> 'mysql',
-    'hostname'	=> 'localhost',
+    'hostname'	=> 'sql12.freesqldatabase.com',
     'port'		=> '3306',
-    'username'	=> 'root',
-    'password'	=> '',
-    'database'	=> 'mockdata',
+    'username'	=> 'sql12798748',
+    'password'	=> 'j6uay4fAX9',
+    'database'	=> 'sql12798748',
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
